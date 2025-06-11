93c4469a5cb060d8f62e2b0892599782089371fd
Ishita Gupta <66175970+ishitaaagupta@users.noreply.github.com>
2025-06-11 11:24:50 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import Head from 'next/head'
 import { ThemeProvider } from "./components/ThemeProvider"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -18,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+    <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className={`${inter.className} antialiased`}>
         <ThemeProvider
           attribute="class"
